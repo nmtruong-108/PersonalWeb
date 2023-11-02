7e0c6793fbea3257521f683238e85c76438c0849
nmtruong-108 <117742458+nmtruong-108@users.noreply.github.com>
2023-11-02 23:23:47 +0700

    update
    ok

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+/*
+// Get all exercise frames
+const exerciseFrames = document.querySelectorAll('.exercise-frame');
+
+// Function to handle click event on exercise frames
+function handleExerciseFrameClick(event) {
+    // Get the link from the exercise frame's href attribute
+    const exerciseLink = event.currentTarget.getAttribute('href');
+
+    // Open the exercise link in a new tab/window
+    window.open(exerciseLink, '_blank');
+
+    // Prevent the default navigation behavior
+    event.preventDefault();
+}
+
+// Add click event listener to each exercise frame
+exerciseFrames.forEach(function(frame) {
+    frame.addEventListener('click', handleExerciseFrameClick);
+});
+*/
